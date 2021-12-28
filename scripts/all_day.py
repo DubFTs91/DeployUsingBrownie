c1abb85c015dcc024a98e941d616ef93b74f3a28
DUBFTs91 <williamshousehold25@gmail.com>
2021-12-27 17:15:26 -0800

    Configured for different networks

--- scripts/all_day.py
+++ scripts/all_day.py
@@ -1,23 +1,31 @@
-from brownie import accounts, config, network
+from brownie import accounts, config, network, MockV3Aggregator
+from web3 import Web3
+
+LOCAL_BLOCKCHAIN_ENV = ["development", "ganache-local"]
+
+DECIMALS = 18
+INITIAL_PRICE = 2000
 
 
 def get_account():
-    if network.show_active() == "development":
-        return accounts[0]
 
+    # Account from local Ganache w/ logging in build > deployments
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENV:
+        return accounts[0]
+    # Account for mainnet PW protected
+    elif network.show_active() == "mainnet":
+        return accounts.load("mainnet-eth")
+    # Account for testnet
     else:
         return accounts.add(config["wallets"]["from_key"])
 
-    # Using default Ganache Account
-    # account = accounts[0]
 
-    # For Real Acct
-    # Using Brownie Accounts
-    # Pre Requirement. Terminal brownie accounts new account-name
-    # account = accounts.load("dubRinkby-account")
-
-    # for test Acct
-    # env variable
-    # account = accounts.add(config["wallets"]["from_key"])
-
-    # print(account)
+# Deploy mock if testing Locally
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying Mocks. . .")
+    if len(MockV3Aggregator) < 1:
+        mock_aggregator = MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(INITIAL_PRICE, "ether"), {"from": get_account()}
+        )
+    print("Mock Deployed")
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,29 +1,25 @@
 from brownie import accounts, config, MyStorage, network, FundMe, MockV3Aggregator
-from scripts.all_day import get_account
+from scripts.all_day import deploy_mocks, get_account, LOCAL_BLOCKCHAIN_ENV
 
 
 def deploy_fund_me():
     account = get_account()
 
-    if network.show_active != "developement":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENV:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
 
     else:
-        # BUG HERE
-        print(f"The active network is {network.show_active()}")
-        print("Deploying Mocks. . .")
-        mock_aggregator = MockV3Aggregator.deploy(
-            18, 2000000000000000000000, {"from": account}
-        )
-        price_feed_address = mock_aggregator.address
-        print("Mock Deployed")
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+
+    print("Deploying Contract. . .")
 
     fund_me = FundMe.deploy(
         price_feed_address,
         {"from": account},
-        publish_source=True,
+        publish_source=config["networks"][network.show_active()]["verify"],
     )
     print(f"Contract deployed to {fund_me.address}")
 
@@ -43,14 +39,6 @@ def deploy_my_storage():
     print(current_val)
 
 
-def get_account():
-    if network.show_active() == "development":
-        return accounts[0]
-
-    else:
-        return accounts.add(config["wallets"]["from_key"])
-
-
 def main():
-    # deploy_my_storage()
+    deploy_my_storage()
     deploy_fund_me()
